0b5aa5923ad92c66d353918a4cf3ae120dbd6803
Heemale <xxx@qq.com>
2024-12-22 17:16:51 +0800

    feat(sdk): Update constants.

--- sdk/wallpaper-sdk/src/utils/constants.ts
+++ sdk/wallpaper-sdk/src/utils/constants.ts
@@ -1,16 +1,16 @@
 export const PACKAGE_ID =
-  '';
+  '0x89c0b0526bc8fde8d9098c80e290d70f6dfad4b55da959698311dc5f1e89119c';
 
 export const UPGRADE_CAP =
-  '';
+  '0xe90e54e6bae8fdaf186586f3fbf079f486e29e6efec0053932111d766dfb8853';
 
 export const PUBLISHER =
-  '';
+  '0x9e26ad9cef052ae38e7641b1a0fcbdcb98d0d53b5cd96d34a3ee0873e9688f77';
 
 export const STATE =
-  '';
+  '0xfc6693ac6d04e449578e29b9a856341569416fbf9b6a5fd3513a7a0bdf6134be';
 
 export const SITE_DISPLAY =
-  '';
+  '0x7fa5f94cbf1d0e27f1ca0af499c66c7acf659e0f4405ccd83da05a1286b7ba04';
 
 export * from './constants';
